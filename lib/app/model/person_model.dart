d3cdbae43a58a2cfffeb601513311198ce87ea64
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-20 10:38:36 -0300

    feat: add new constructor at model

--- lib/app/model/person_model.dart
+++ lib/app/model/person_model.dart
@@ -12,4 +12,14 @@ class PersonModel {
     required this.dataAdmissao,
     required this.telefone,
   });
+
+  factory PersonModel.fromJson(Map<String, dynamic> json) {
+    return PersonModel(
+      image: json['image'],
+      name: json['name'],
+      cargo: json['cargo'],
+      dataAdmissao: json['dataAdmissao'],
+      telefone: json['telefone'],
+    );
+  }
 }
