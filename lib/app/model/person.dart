0cc9a15fbf2854b4195884562f66d64b257a1f6d
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-19 23:50:33 -0300

    feat: adding the design system

---
+++ lib/app/model/person.dart
@@ -0,0 +1,15 @@
+class Person {
+  final String image;
+  final String name;
+  final String cargo;
+  final String dataAdmissao;
+  final String telefone;
+
+  Person({
+    required this.image,
+    required this.name,
+    required this.cargo,
+    required this.dataAdmissao,
+    required this.telefone,
+  });
+}
